6806bc04b7c969291d3cab29161823c8ab67951d
Brendan Drew <bdrew@linkedin.com>
2015-10-26 12:16:19 -0700

    OFFREL-517: Add support for bootstrapping; initial commit covers bootstrapping, subsequent commits will integrate reporting
    RB=597521
    BUG=OFFREL-517
    G=photon-reviewers
    A=jfleming

---
+++ photon-ml/src/main/scala/com/linkedin/photon/ml/supervised/model/CoefficientSummary.scala
@@ -0,0 +1,45 @@
+package com.linkedin.photon.ml.supervised.model
+
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics
+import org.apache.commons.math3.stat.descriptive.rank.PSquarePercentile
+
+/**
+ * Miscellaneous things that describe a GLM coefficient.
+ *
+ * Notes for testing:
+ * <ul>
+ *   <li>Tests should assume that the Apache Commons stuff is correct -- really just need to establish that
+ *   things have been reasonably wired.</li>
+ * </ul>
+ */
+class CoefficientSummary extends Serializable {
+  private val summary:SummaryStatistics = new SummaryStatistics()
+  private val quantiles:Array[PSquarePercentile] = Array[PSquarePercentile](new PSquarePercentile(0.25),
+    new PSquarePercentile(0.50), new PSquarePercentile(0.75))
+
+  def accumulate(x:Double): Unit = {
+    summary.addValue(x)
+    quantiles.foreach { y => y.increment(x) }
+  }
+
+  def getMean(): Double = summary.getMean
+
+  def getMin(): Double = summary.getMin
+
+  def getMax(): Double = summary.getMax
+
+  def getStdDev(): Double = summary.getStandardDeviation
+
+  def estimateFirstQuartile(): Double = quantiles(0).quantile()
+
+  def estimateMedian(): Double = quantiles(1).quantile()
+
+  def estimateThirdQuartile(): Double = quantiles(2).quantile()
+
+  def getCount(): Long = summary.getN
+
+  override def toString():String = {
+    f"Range: [Min: $getMin%.03f, Q1: $estimateFirstQuartile%.03f, Med: $estimateMedian%.03f, Q3: $estimateThirdQuartile%.03f, Max: $getMax%.03f) " +
+    f"Mean: [$getMean%.03f], Std. Dev.[$getStdDev%.03f], # samples = [$getCount]"
+  }
+}
