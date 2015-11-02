39e16416b7d5fd82e778e060d15b098b07dde0fa
Brendan Drew <bdrew@linkedin.com>
2015-11-02 15:31:55 -0800

    OFFREL-740: improve summarization performance by changing how E[|feature|] is computed to better leverage the underlying Spark summary
    RB=603223
    G=photon-reviewers
    A=dpeng

--- photon-ml/src/main/scala/com/linkedin/photon/ml/stat/BasicStatistics.scala
+++ photon-ml/src/main/scala/com/linkedin/photon/ml/stat/BasicStatistics.scala
@@ -1,6 +1,5 @@
 package com.linkedin.photon.ml.stat
 
-import breeze.linalg.DenseVector
 import breeze.linalg.Vector
 import com.linkedin.photon.ml.data
 import com.linkedin.photon.ml.data.LabeledPoint
@@ -24,46 +23,8 @@ private[ml] object BasicStatistics {
    */
   def getBasicStatistics(inputData: RDD[LabeledPoint]): BasicStatisticalSummary = {
     val mllibSummary = Statistics.colStats(inputData.map(x => VectorsWrapper.breezeToMllib(x.features)))
-
-    val length = inputData.take(1)(0).features.length
-    val zero = (0L, DenseVector.zeros[Long](length), DenseVector.zeros[Double](length))
-
-    val (totalSamples, nonzeroCount, nonzeroMean) = inputData.mapPartitions(x => {
-      if (x.hasNext) {
-        var count = 1L
-        val one = x.take(1).next()
-        val sums:Vector[Double] = one.features.copy
-        val counts:Vector[Long] = sums.mapValues(x => if (math.abs(x) > 1e-12) 1L else 0L).toDenseVector
-
-        x.foreach(y => {
-          count += 1
-          y.features.iterator.foreach(f => {
-            val q = math.abs(f._2)
-            counts(f._1) += 1
-            sums(f._1) += (q - sums(f._1))/counts(f._1)
-            (f._1, 0.0)
-          })
-        })
-
-        Seq((count, counts, sums)).iterator
-      } else {
-        Seq.empty.iterator
-      }
-    }).fold(zero)(op = (x, y) => {
-      val totalCount = x._1 + y._1
-      val newCounts = x._2 + y._2
-      val delta = y._3 - x._3
-      val scales = y._2.mapValues(x => x.toDouble) :/ newCounts.mapValues(x => if (x != 0) x.toDouble else 1.0)
-      val newMeans = x._3 + delta :* scales
-      (totalCount, newCounts, newMeans)
-    })
-
-    var meanAbs:Vector[Double] = DenseVector.zeros[Double](length)
-
-    if (totalSamples > 0L) {
-      meanAbs = nonzeroMean :* (totalSamples - nonzeroCount).mapValues(x => 1.0 - x.toDouble / totalSamples)
-    }
-
+    val scale = if (mllibSummary.count > 0) { mllibSummary.count.toDouble } else { 1.0 }
+    val meanAbs:Vector[Double] = VectorsWrapper.mllibToBreeze(mllibSummary.normL1) / scale
     BasicStatisticalSummary(mllibSummary, meanAbs)
   }
 
