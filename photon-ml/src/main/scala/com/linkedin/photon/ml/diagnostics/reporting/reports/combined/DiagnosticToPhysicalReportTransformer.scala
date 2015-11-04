b0ddb65abd3b155fb64ccec239d19d260149433d
Brendan Drew <bdrew@linkedin.com>
2015-11-04 09:22:27 -0800

    OFFREL-720: Add summary section and plot per-lambda metrics
    RB=604953
    BUG=OFFREL-720
    G=photon-reviewers
    R=rmelick
    A=nkatariy

--- photon-ml/src/main/scala/com/linkedin/photon/ml/diagnostics/reporting/reports/combined/DiagnosticToPhysicalReportTransformer.scala
+++ photon-ml/src/main/scala/com/linkedin/photon/ml/diagnostics/reporting/reports/combined/DiagnosticToPhysicalReportTransformer.scala
@@ -3,10 +3,11 @@ package com.linkedin.photon.ml.diagnostics.reporting.reports.combined
 import java.text.SimpleDateFormat
 import java.util.{Date, TimeZone, Calendar}
 
-import com.linkedin.photon.ml.diagnostics.reporting.{ChapterPhysicalReport, DocumentPhysicalReport, LogicalToPhysicalReportTransformer}
-import com.linkedin.photon.ml.diagnostics.reporting.reports.model.ModelDiagnosticToPhysicalReportTransformer
+import com.linkedin.photon.ml.diagnostics.reporting._
+import com.linkedin.photon.ml.diagnostics.reporting.reports.model.{ModelDiagnosticReport, ModelDiagnosticToPhysicalReportTransformer}
 import com.linkedin.photon.ml.diagnostics.reporting.reports.system.SystemToPhysicalReportTransformer
 import com.linkedin.photon.ml.supervised.model.GeneralizedLinearModel
+import com.xeiam.xchart.{StyleManager, ChartBuilder}
 
 /**
  * Transform diagnostic reports into their physical report representation
@@ -18,8 +19,10 @@ class DiagnosticToPhysicalReportTransformer extends LogicalToPhysicalReportTrans
   def transform(diag: DiagnosticReport): DocumentPhysicalReport = {
     val formatter = new SimpleDateFormat()
     val now = new Date()
+    val summary = transformSummary(diag.modelReports)
     new DocumentPhysicalReport(
       Seq(
+        new ChapterPhysicalReport(Seq(summary), "Summary"),
         SYSTEM_CHAPTER_TRANSFORMER.transform(diag.systemReport),
         new ChapterPhysicalReport
         (diag.modelReports.toArray.sortBy(x => x.lambda).map(x => MODEL_SECTION_TRANSFORMER.transform(x)).toSeq, MODEL_CHAPTER_TITLE)),
@@ -31,4 +34,52 @@ object DiagnosticToPhysicalReportTransformer {
   val SYSTEM_CHAPTER_TRANSFORMER = new SystemToPhysicalReportTransformer()
   val MODEL_SECTION_TRANSFORMER = new ModelDiagnosticToPhysicalReportTransformer[GeneralizedLinearModel]()
   val MODEL_CHAPTER_TITLE = "Detailed Model Diagnostics"
+  val MODEL_SUMMARY_CHAPTER = "Summary"
+  val MODEL_METRICS_SUMMARY = "Model Metrics"
+
+  /**
+   * Generate a summary section which includes a quick description of which lambdas did best/worst on a particular
+   * metric and a visual summary of models by metric.
+   *
+   * @param models
+   * @return
+   */
+  private def transformSummary(models:Seq[ModelDiagnosticReport[GeneralizedLinearModel]]): SectionPhysicalReport = {
+    val metricsByLambda = models.flatMap(x => {
+      x.metrics.map(y => {
+        (y._1, (x.lambda, y._2))
+      }).iterator
+    }).groupBy(_._1).mapValues(_.map(_._2).toMap)
+
+    val metrics = metricsByLambda.keys.toSeq.sorted
+    val bestModelByMetric = new BulletedListPhysicalReport(
+      metrics.flatMap(metric => {
+        metricsByLambda.get(metric).toSeq.flatMap(values => {
+          val sorted = values.toSeq.sortBy(_._2)
+          val minLambda = sorted.head
+          val maxLambda = sorted.last
+
+          Seq(new SimpleTextPhysicalReport(s"Metric ${metric} min ${minLambda._2} @ lambda = ${minLambda._1}, max ${maxLambda._2} @ lambda = ${maxLambda._1}")).iterator
+        }).iterator
+      }))
+
+      val modelMetricPlots = metrics.flatMap(metric => {
+        metricsByLambda.get(metric).toSeq.flatMap(lambdaToMetric => {
+          val sortedByLambda = lambdaToMetric.toSeq.sortBy(_._1)
+          val builder = new ChartBuilder()
+          val chart = builder.chartType(StyleManager.ChartType.Bar)
+            .height(PlotUtils.PLOT_HEIGHT)
+            .theme(StyleManager.ChartTheme.XChart)
+            .title(metric)
+            .width(PlotUtils.PLOT_WIDTH)
+            .build()
+          sortedByLambda.foreach(x => {
+            chart.addSeries(s"Lambda = ${x._1}", Array(1.0), Array(x._2))
+          })
+          Seq(new PlotPhysicalReport(chart)).iterator
+        }).iterator
+      })
+
+    new SectionPhysicalReport(Seq(bestModelByMetric) ++ modelMetricPlots, MODEL_SUMMARY_CHAPTER)
+  }
 }
