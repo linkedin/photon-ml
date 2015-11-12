165795211eb34bb6fd2df59d136d87a6c6b29114
Yitong Zhou <yizhou@linkedin.com>
2015-11-11 16:53:22 -0800

    OFFREL-542: Add License Part 3. Append license to all files.
    RB=611160
    G=photon-reviewers
    A=pogilvie

--- dev-scripts/add_license.sh
+++ dev-scripts/add_license.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Copyright 2014 LinkedIn Corp. All rights reserved.
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain a
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain a
 # copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
@@ -39,7 +39,7 @@ pyRsh_files=(`find . -regex '.*\.py\|.*\.sh\|.*\.R'`)
 pyRsh_license=.pyRsh_apache_license.txt
 rm -f $pyRsh_license
 # Surround License with Python/R comment style
-sed -e 's/^/# /' LICENSE > $pyRsh_license
+sed -e 's/^/# /' -e 's/ $//' LICENSE > $pyRsh_license
 
 # Apply license to java files
 apply_license ${java_license} ${java_files[@]}
