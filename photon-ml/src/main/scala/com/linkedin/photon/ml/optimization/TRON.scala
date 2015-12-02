8003b2eb3c981bdffd33526060d7bdaf3d56ee87
Yitong Zhou <yizhou@linkedin.com>
2015-12-02 12:17:54 -0800

    [opensource photon-ml] OFFREL-833: append copyright and license notice for TRON
    RB=622774
    G=photon-reviewers
    A=xazhang

--- photon-ml/src/main/scala/com/linkedin/photon/ml/optimization/TRON.scala
+++ photon-ml/src/main/scala/com/linkedin/photon/ml/optimization/TRON.scala
@@ -12,6 +12,43 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
+
+/*
+ * NOTE: The codes are heavily influenced by SPARK LIBLINEAR TRON implementation,
+ * though not an exact copy. It also subject to the LIBLINEAR project's license
+ * and copyright notice:
+ *
+ * Copyright (c) 2007-2015 The LIBLINEAR Project.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither name of copyright holders nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.linkedin.photon.ml.optimization
 
 import breeze.linalg.{Vector, norm}
@@ -23,14 +60,9 @@ import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
 
 /**
- * Class used to solve an optimization problem using trust region Newton method (TRON).
+ * This class used to solve an optimization problem using trust region Newton method (TRON).
  * Reference1: [[http://www.csie.ntu.edu.tw/~cjlin/papers/logistic.pdf]]
  * Reference2: [[http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/distributed-liblinear/spark/running_spark_liblinear.html]]
- *
- * Note that I tried to stick as close as possible to the Spark Liblinear source code, for the following
- * considerations: 1. maintainability (patches for Spark Liblinear can be applied easily), 2. to avoid introduce new
- * bugs, 3. code reviews (the source code here can be compared to Spark Liblinear).
- *
  * @tparam Datum Generic type of input data point
  * @author xazhang
  * @author dpeng
