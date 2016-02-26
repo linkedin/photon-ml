af301acf689c05aba7059eb62c6e025c5b511de4
Joshua Fleming <jfleming@linkedin.com>
2016-02-26 10:41:17 -0500

    Refactor the relationship between Algorithms and Optimizers
    This change shifts responsibility of creating optimizers to
    GeneralLinearAlgorithm, and allows for much more flexibility in defining
    new algorithms.
    
    The problem is described in more detail here:
    
    https://docs.google.com/a/linkedin.com/document/d/1sVX7z_yh5qV36636yr7Fkom0o9s1GqHvzxMVkE9Y9hc/edit?usp=sharing
    
    NOTE: This does not implement the proposed solution there. I started on
    that path, but it became a much larger project. In order to get going
    with the Game migration, I'm making this less invasive fix.
    
    https://jira01.corp.linkedin.com:8443/browse/ML-1023
    
    RB=670039
    G=photon-reviewers
    R=yizhou,abain,xazhang,dpeng,pogilvie,bchen,nkatariy
    A=xazhang,ashelkov
    
    
    Former-commit-id: a51bbafb701aa0da6979274254f43c93cae279f1

--- photon-ml/src/main/scala/com/linkedin/photon/ml/supervised/classification/LogisticRegressionAlgorithm.scala
+++ photon-ml/src/main/scala/com/linkedin/photon/ml/supervised/classification/LogisticRegressionAlgorithm.scala
@@ -4,7 +4,7 @@ import breeze.linalg.Vector
 import com.linkedin.photon.ml.data.{LabeledPoint, ObjectProvider}
 import com.linkedin.photon.ml.function.{LogisticLossFunction, TwiceDiffFunction}
 import com.linkedin.photon.ml.normalization.NormalizationContext
-import com.linkedin.photon.ml.optimization.{LBFGS, RegularizationContext}
+import com.linkedin.photon.ml.optimization.{LBFGS, Optimizer, OptimizerConfig, OptimizerFactory, RegularizationContext}
 import com.linkedin.photon.ml.supervised.model.GeneralizedLinearAlgorithm
 
 /**
@@ -28,6 +28,17 @@ class LogisticRegressionAlgorithm
     val basicFunction = new LogisticLossFunction(normalizationContext)
     basicFunction.treeAggregateDepth = treeAggregateDepth
     TwiceDiffFunction.withRegularization(basicFunction, regularizationContext, regularizationWeight)
+  }
+
+  /**
+   * Create an Optimizer according to the config.
+   *
+   * @param optimizerConfig Optimizer configuration
+   * @return A new Optimizer created according to the configuration
+   */
+  override protected def createOptimizer(
+      optimizerConfig: OptimizerConfig): Optimizer[LabeledPoint, TwiceDiffFunction[LabeledPoint]] = {
+    OptimizerFactory.twiceDiffOptimizer(optimizerConfig)
   }
 
   /**
