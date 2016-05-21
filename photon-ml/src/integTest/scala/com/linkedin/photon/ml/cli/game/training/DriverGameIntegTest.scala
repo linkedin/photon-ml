113ed8b5c90fcbba99902b655be2399f87882586
Alex Shelkovnykov <ashelkov@ashelkov-ld1.linkedin.biz>
2016-05-20 17:18:55 -0700

    Deprecate 'save-models-to-hdfs'. Add option to output all, best, or no models.

--- photon-ml/src/integTest/scala/com/linkedin/photon/ml/cli/game/training/DriverGameIntegTest.scala
+++ photon-ml/src/integTest/scala/com/linkedin/photon/ml/cli/game/training/DriverGameIntegTest.scala
@@ -15,19 +15,20 @@
 package com.linkedin.photon.ml.cli.game.training
 
 import collection.JavaConversions._
-import java.nio.file.{Files, FileSystems, Path}
-import org.apache.spark.SparkConf
-import org.testng.annotations.Test
-import org.testng.Assert._
-
+import com.linkedin.photon.ml.SparkContextConfiguration
 import com.linkedin.photon.ml.avro.AvroIOUtils
 import com.linkedin.photon.ml.avro.generated.BayesianLinearModelAvro
 import com.linkedin.photon.ml.avro.model.ModelProcessingUtils
 import com.linkedin.photon.ml.data.{FixedEffectDataSet, RandomEffectDataSet}
-import com.linkedin.photon.ml.SparkContextConfiguration
+import com.linkedin.photon.ml.io.ModelOutputMode
 import com.linkedin.photon.ml.supervised.TaskType
 import com.linkedin.photon.ml.test.{CommonTestUtils, SparkTestUtils, TestTemplateWithTmpDir}
 import com.linkedin.photon.ml.util.PhotonLogger
+import org.apache.spark.SparkConf
+import org.testng.annotations.Test
+import org.testng.Assert._
+
+import java.nio.file.{Files, FileSystems, Path}
 
 class DriverGameIntegTest extends SparkTestUtils with TestTemplateWithTmpDir {
   import DriverGameIntegTest._
@@ -36,33 +37,57 @@ class DriverGameIntegTest extends SparkTestUtils with TestTemplateWithTmpDir {
   @Test
   def testFixedEffects() = sparkTest("fixedEffects", useKryo = true) {
     val outputDir = s"$getTmpDir/fixedEffects"
-
     // This is a baseline RMSE capture from an assumed-correct implementation on 4/14/2016
     val errorThreshold = 1.7
+    val driver = runDriver(argArray(fixedEffectArgs ++ Map("output-dir" -> outputDir)))
+    val allFixedEffectModelPath = allModelPath(outputDir, "fixed-effect", "shard1")
+    val bestFixedEffectModelPath = bestModelPath(outputDir, "fixed-effect", "shard1")
 
-    val driver = runDriver(argArray(fixedEffectArgs ++ Map(
-      "output-dir" -> outputDir)))
-
-    val fixedEffectModelPath = modelPath(outputDir, "fixed-effect", "shard1")
-
-    assertTrue(Files.exists(fixedEffectModelPath))
-    assertTrue(modelSane(fixedEffectModelPath, expectedNumCoefficients = 14983))
+    println("HERE")
+    println(allFixedEffectModelPath)
+    assertTrue(Files.exists(allFixedEffectModelPath))
+    assertTrue(Files.exists(bestFixedEffectModelPath))
+    assertTrue(modelSane(allFixedEffectModelPath, expectedNumCoefficients = 14983))
+    assertTrue(modelSane(bestFixedEffectModelPath, expectedNumCoefficients = 14983))
+    assertTrue(evaluateModel(driver, fs.getPath(outputDir, "all/0")) < errorThreshold)
     assertTrue(evaluateModel(driver, fs.getPath(outputDir, "best")) < errorThreshold)
+  }
+
+  @Test
+  def testSaveBestOnly() = sparkTest("saveBestOnly", useKryo = true) {
+    val outputDir = s"$getTmpDir/fixedEffects"
+    val driver = runDriver(argArray(fixedEffectArgs ++ Map(
+      "output-dir" -> outputDir,
+      "model-output-mode" -> ModelOutputMode.BEST.toString)))
+    val allFixedEffectModelPath = allModelPath(outputDir, "fixed-effect", "shard1")
+    val bestFixedEffectModelPath = bestModelPath(outputDir, "fixed-effect", "shard1")
+
+    assertFalse(Files.exists(allFixedEffectModelPath))
+    assertTrue(Files.exists(bestFixedEffectModelPath))
+  }
+
+  @Test
+  def testSaveNone() = sparkTest("saveNone", useKryo = true) {
+    val outputDir = s"$getTmpDir/fixedEffects"
+    val driver = runDriver(argArray(fixedEffectArgs ++ Map(
+      "output-dir" -> outputDir,
+      "model-output-mode" -> ModelOutputMode.NONE.toString)))
+    val allFixedEffectModelPath = allModelPath(outputDir, "fixed-effect", "shard1")
+    val bestFixedEffectModelPath = bestModelPath(outputDir, "fixed-effect", "shard1")
+
+    assertFalse(Files.exists(allFixedEffectModelPath))
+    assertFalse(Files.exists(bestFixedEffectModelPath))
   }
 
   @Test
   def testRandomEffects() = sparkTest("randomEffects", useKryo = true) {
     val outputDir = s"$getTmpDir/randomEffects"
-
     // This is a baseline RMSE capture from an assumed-correct implementation on 4/14/2016
     val errorThreshold = 2.2
-
-    val driver = runDriver(argArray(randomEffectArgs ++ Map(
-      "output-dir" -> outputDir)))
-
-    val userModelPath = modelPath(outputDir, "random-effect", "userId-shard2")
-    val songModelPath = modelPath(outputDir, "random-effect", "songId-shard3")
-    val artistModelPath = modelPath(outputDir, "random-effect", "artistId-shard3")
+    val driver = runDriver(argArray(randomEffectArgs ++ Map("output-dir" -> outputDir)))
+    val userModelPath = bestModelPath(outputDir, "random-effect", "userId-shard2")
+    val songModelPath = bestModelPath(outputDir, "random-effect", "songId-shard3")
+    val artistModelPath = bestModelPath(outputDir, "random-effect", "artistId-shard3")
 
     assertTrue(Files.exists(userModelPath))
     assertTrue(modelSane(userModelPath, expectedNumCoefficients = 21))
@@ -86,10 +111,10 @@ class DriverGameIntegTest extends SparkTestUtils with TestTemplateWithTmpDir {
     val driver = runDriver(argArray(fixedAndRandomEffectArgs ++ Map(
       "output-dir" -> outputDir)))
 
-    val fixedEffectModelPath = modelPath(outputDir, "fixed-effect", "shard1")
-    val userModelPath = modelPath(outputDir, "random-effect", "userId-shard2")
-    val songModelPath = modelPath(outputDir, "random-effect", "songId-shard3")
-    val artistModelPath = modelPath(outputDir, "random-effect", "artistId-shard3")
+    val fixedEffectModelPath = bestModelPath(outputDir, "fixed-effect", "shard1")
+    val userModelPath = bestModelPath(outputDir, "random-effect", "userId-shard2")
+    val songModelPath = bestModelPath(outputDir, "random-effect", "songId-shard3")
+    val artistModelPath = bestModelPath(outputDir, "random-effect", "artistId-shard3")
 
     assertTrue(Files.exists(fixedEffectModelPath))
     assertTrue(modelSane(fixedEffectModelPath, expectedNumCoefficients = 15017))
@@ -133,75 +158,75 @@ class DriverGameIntegTest extends SparkTestUtils with TestTemplateWithTmpDir {
 
   @Test
   def testPrepareFixedAndRandomEffectTrainingDataSet() =
-      sparkTest("prepareFixedAndRandomEffectTrainingDataSet", useKryo = true) {
-    val outputDir = s"$getTmpDir/prepareFixedEffectTrainingDataSet"
+    sparkTest("prepareFixedAndRandomEffectTrainingDataSet", useKryo = true) {
+      val outputDir = s"$getTmpDir/prepareFixedEffectTrainingDataSet"
 
-    val args = argArray(fixedAndRandomEffectArgs ++ Map(
-      "output-dir" -> outputDir))
+      val args = argArray(fixedAndRandomEffectArgs ++ Map(
+        "output-dir" -> outputDir))
 
-    val driver = new Driver(
-      Params.parseFromCommandLine(args), sc, new PhotonLogger(s"$outputDir/log", sc))
+      val driver = new Driver(
+        Params.parseFromCommandLine(args), sc, new PhotonLogger(s"$outputDir/log", sc))
 
-    val featureShardIdToFeatureMapMap = driver.prepareFeatureMaps()
-    val gameDataSet = driver.prepareGameDataSet(featureShardIdToFeatureMapMap)
-    val trainingDataSet = driver.prepareTrainingDataSet(gameDataSet)
+      val featureShardIdToFeatureMapMap = driver.prepareFeatureMaps()
+      val gameDataSet = driver.prepareGameDataSet(featureShardIdToFeatureMapMap)
+      val trainingDataSet = driver.prepareTrainingDataSet(gameDataSet)
 
-    assertEquals(trainingDataSet.size, 4)
+      assertEquals(trainingDataSet.size, 4)
 
-    // fixed effect data
-    trainingDataSet("global") match {
-      case ds: FixedEffectDataSet =>
-        assertEquals(ds.labeledPoints.count(), 34810)
-        assertEquals(ds.numFeatures, 30085)
+      // fixed effect data
+      trainingDataSet("global") match {
+        case ds: FixedEffectDataSet =>
+          assertEquals(ds.labeledPoints.count(), 34810)
+          assertEquals(ds.numFeatures, 30085)
 
-      case _ => fail("Wrong dataset type.")
+        case _ => fail("Wrong dataset type.")
+      }
+
+      // per-user data
+      trainingDataSet("per-user") match {
+        case ds: RandomEffectDataSet =>
+          assertEquals(ds.activeData.count(), 33110)
+
+          val featureStats = ds.activeData.values.map(_.numActiveFeatures).stats()
+          assertEquals(featureStats.count, 33110)
+          assertEquals(featureStats.mean, 24.12999, tol)
+          assertEquals(featureStats.stdev, 0.611194, tol)
+          assertEquals(featureStats.max, 40.0, tol)
+          assertEquals(featureStats.min, 24.0, tol)
+
+        case _ => fail("Wrong dataset type.")
+      }
+
+      // per-song data
+      trainingDataSet("per-song") match {
+        case ds: RandomEffectDataSet =>
+          assertEquals(ds.activeData.count(), 23167)
+
+          val featureStats = ds.activeData.values.map(_.numActiveFeatures).stats()
+          assertEquals(featureStats.count, 23167)
+          assertEquals(featureStats.mean, 21.0, tol)
+          assertEquals(featureStats.stdev, 0.0, tol)
+          assertEquals(featureStats.max, 21.0, tol)
+          assertEquals(featureStats.min, 21.0, tol)
+
+        case _ => fail("Wrong dataset type.")
+      }
+
+      // per-artist data
+      trainingDataSet("per-artist") match {
+        case ds: RandomEffectDataSet =>
+          assertEquals(ds.activeData.count(), 4471)
+
+          val featureStats = ds.activeData.values.map(_.numActiveFeatures).stats()
+          assertEquals(featureStats.count, 4471)
+          assertEquals(featureStats.mean, 3.0, tol)
+          assertEquals(featureStats.stdev, 0.0, tol)
+          assertEquals(featureStats.max, 3.0, tol)
+          assertEquals(featureStats.min, 3.0, tol)
+
+        case _ => fail("Wrong dataset type.")
+      }
     }
-
-    // per-user data
-    trainingDataSet("per-user") match {
-      case ds: RandomEffectDataSet =>
-        assertEquals(ds.activeData.count(), 33110)
-
-        val featureStats = ds.activeData.values.map(_.numActiveFeatures).stats()
-        assertEquals(featureStats.count, 33110)
-        assertEquals(featureStats.mean, 24.12999, tol)
-        assertEquals(featureStats.stdev, 0.611194, tol)
-        assertEquals(featureStats.max, 40.0, tol)
-        assertEquals(featureStats.min, 24.0, tol)
-
-      case _ => fail("Wrong dataset type.")
-    }
-
-    // per-song data
-    trainingDataSet("per-song") match {
-      case ds: RandomEffectDataSet =>
-        assertEquals(ds.activeData.count(), 23167)
-
-        val featureStats = ds.activeData.values.map(_.numActiveFeatures).stats()
-        assertEquals(featureStats.count, 23167)
-        assertEquals(featureStats.mean, 21.0, tol)
-        assertEquals(featureStats.stdev, 0.0, tol)
-        assertEquals(featureStats.max, 21.0, tol)
-        assertEquals(featureStats.min, 21.0, tol)
-
-      case _ => fail("Wrong dataset type.")
-    }
-
-    // per-artist data
-    trainingDataSet("per-artist") match {
-      case ds: RandomEffectDataSet =>
-        assertEquals(ds.activeData.count(), 4471)
-
-        val featureStats = ds.activeData.values.map(_.numActiveFeatures).stats()
-        assertEquals(featureStats.count, 4471)
-        assertEquals(featureStats.mean, 3.0, tol)
-        assertEquals(featureStats.stdev, 0.0, tol)
-        assertEquals(featureStats.max, 3.0, tol)
-        assertEquals(featureStats.min, 3.0, tol)
-
-      case _ => fail("Wrong dataset type.")
-    }
-  }
 
   @Test
   def testMultipleOptimizerConfigs() = sparkTest("multipleOptimizerConfigs", useKryo = true) {
@@ -214,9 +239,9 @@ class DriverGameIntegTest extends SparkTestUtils with TestTemplateWithTmpDir {
       "output-dir" -> outputDir,
       "fixed-effect-optimization-configurations" ->
         ("global:10,1e-5,10,1,tron,l2;" +
-         "global:10,1e-5,10,1,lbfgs,l2"))))
+          "global:10,1e-5,10,1,lbfgs,l2"))))
 
-    val fixedEffectModelPath = modelPath(outputDir, "fixed-effect", "shard1")
+    val fixedEffectModelPath = bestModelPath(outputDir, "fixed-effect", "shard1")
 
     assertTrue(Files.exists(fixedEffectModelPath))
     assertTrue(modelSane(fixedEffectModelPath, expectedNumCoefficients = 14982))
@@ -224,11 +249,11 @@ class DriverGameIntegTest extends SparkTestUtils with TestTemplateWithTmpDir {
   }
 
   /**
-   * Overridden spark test provider that allows for specifying whether to use kryo serialization
-   *
-   * @param name the test job name
-   * @param body the execution closure
-   */
+    * Overridden spark test provider that allows for specifying whether to use kryo serialization
+    *
+    * @param name the test job name
+    * @param body the execution closure
+    */
   def sparkTest(name: String, useKryo: Boolean)(body: => Unit) {
     SparkTestUtils.SPARK_LOCAL_CONFIG.synchronized {
       sc = SparkContextConfiguration.asYarnClient(
@@ -245,26 +270,27 @@ class DriverGameIntegTest extends SparkTestUtils with TestTemplateWithTmpDir {
   }
 
   /**
-   * Perform a very basic sanity check on the model
-   *
-   * @param path path to the model coefficients file
-   * @return true if the model is sane
-   */
+    * Perform a very basic sanity check on the model
+    *
+    * @param path path to the model coefficients file
+    * @param expectedNumCoefficients expected number of non-zero coefficients
+    * @return true if the model is sane
+    */
   def modelSane(path: Path, expectedNumCoefficients: Int): Boolean = {
     val modelAvro = AvroIOUtils.readFromSingleAvro[BayesianLinearModelAvro](
       sc, path.toString, BayesianLinearModelAvro.getClassSchema.toString)
 
-    val means = modelAvro(0).getMeans()
+    val means = modelAvro.head.getMeans()
     means.filter(x => x.getValue != 0).size == expectedNumCoefficients
   }
 
   /**
-   * Evaluate the model by calculating RMSE between its scores and the validation set
-   *
-   * @param driver the driver instance used for training
-   * @param modelPath base path to the GAME model files
-   * @return validation error for the model
-   */
+    * Evaluate the model by calculating RMSE between its scores and the validation set
+    *
+    * @param driver the driver instance used for training
+    * @param modelPath base path to the GAME model files
+    * @return validation error for the model
+    */
   def evaluateModel(driver: Driver, modelPath: Path): Double = {
     val featureShardIdToFeatureMapMap = driver.prepareFeatureMaps()
     val gameDataSet = driver.prepareGameDataSet(featureShardIdToFeatureMapMap)
@@ -284,10 +310,10 @@ class DriverGameIntegTest extends SparkTestUtils with TestTemplateWithTmpDir {
   }
 
   /**
-   * Run the Game driver with the specified arguments
-   *
-   * @param args the command-line arguments
-   */
+    * Run the Game driver with the specified arguments
+    *
+    * @param args the command-line arguments
+    */
   def runDriver(args: Array[String]) = {
     val params = Params.parseFromCommandLine(args)
     val logger = new PhotonLogger(s"${params.outputDir}/log", sc)
@@ -312,20 +338,19 @@ object DriverGameIntegTest {
   val tol = 1e-5
 
   /**
-   * Default arguments to the Game driver
-   */
+    * Default arguments to the Game driver
+    */
   def defaultArgs = Map(
     "task-type" -> TaskType.LINEAR_REGRESSION.toString,
     "train-input-dirs" -> trainPath,
     "validate-input-dirs" -> testPath,
     "feature-name-and-term-set-path" -> featurePath,
     "num-iterations" -> numIterations.toString,
-    "num-output-files-for-random-effect-model" -> "-1",
-    "save-models-to-hdfs" -> true.toString)
+    "num-output-files-for-random-effect-model" -> "-1")
 
   /**
-   * Default fixed effect arguments
-   */
+    * Default fixed effect arguments
+    */
   def fixedEffectArgs = defaultArgs ++ Map(
     "feature-shard-id-to-feature-section-keys-map" -> "shard1:features",
     "updating-sequence" -> "global",
@@ -339,8 +364,8 @@ object DriverGameIntegTest {
       s"global:shard1,$numPartitionsForFixedEffectDataSet")
 
   /**
-   * Default random effect arguments
-   */
+    * Default random effect arguments
+    */
   def randomEffectArgs = {
     val userRandomEffectRegularizationWeight = 1
     val songRandomEffectRegularizationWeight = 1
@@ -353,19 +378,19 @@ object DriverGameIntegTest {
       // random-effect optimization config
       "random-effect-optimization-configurations" ->
         (s"per-user:10,1e-5,$userRandomEffectRegularizationWeight,1,tron,l2|" +
-         s"per-song:10,1e-5,$songRandomEffectRegularizationWeight,1,tron,l2|" +
-         s"per-artist:10,1e-5,$userRandomEffectRegularizationWeight,1,tron,l2"),
+          s"per-song:10,1e-5,$songRandomEffectRegularizationWeight,1,tron,l2|" +
+          s"per-artist:10,1e-5,$userRandomEffectRegularizationWeight,1,tron,l2"),
 
       // random-effect data config
       "random-effect-data-configurations" ->
         (s"per-user:userId,shard2,$numPartitionsForRandomEffectDataSet,-1,0,-1,index_map|" +
-         s"per-song:songId,shard3,$numPartitionsForRandomEffectDataSet,-1,0,-1,index_map|" +
-         s"per-artist:artistId,shard3,$numPartitionsForRandomEffectDataSet,-1,0,-1,RANDOM=2"))
+          s"per-song:songId,shard3,$numPartitionsForRandomEffectDataSet,-1,0,-1,index_map|" +
+          s"per-artist:artistId,shard3,$numPartitionsForRandomEffectDataSet,-1,0,-1,RANDOM=2"))
   }
 
   /**
-   * Default fixed and random effect arguments
-   */
+    * Default fixed and random effect arguments
+    */
   def fixedAndRandomEffectArgs = {
     fixedEffectArgs ++ randomEffectArgs ++ Map(
       "feature-shard-id-to-feature-section-keys-map" ->
@@ -375,13 +400,36 @@ object DriverGameIntegTest {
   }
 
   /**
-   * Build the path to the model coefficients file, given some model properties
-   *
-   * @param outputDir output base directory
-   * @param modelType model type (e.g. "fixed-effect", "random-effect")
-   * @param shardId the shard id designator
-   * @return full path to model coefficients file
-   */
-  def modelPath(outputDir: String, modelType: String, shardId: String): Path = fs.getPath(
-      outputDir, "best", modelType, shardId, "coefficients", "part-00000.avro")
+    * Build the path to the model coefficients file, given some model properties
+    *
+    * @param outputDir output base directory
+    * @param outputMode output mode (best or all)
+    * @param modelType model type (e.g. "fixed-effect", "random-effect")
+    * @param shardId the shard id designator
+    * @return full path to model coefficients file
+    */
+  private def modelPath(outputDir: String, outputMode: String, modelType: String, shardId: String): Path =
+    fs.getPath(outputDir, outputMode, modelType, shardId, "coefficients", "part-00000.avro")
+
+  /**
+    * Build the path to the model coefficients file
+    *
+    * @param outputDir output base directory
+    * @param modelType model type (e.g. "fixed-effect", "random-effect")
+    * @param shardId the shard id designator
+    * @return full path to model coefficients file
+    */
+  def allModelPath(outputDir: String, modelType: String, shardId: String): Path =
+    modelPath(outputDir, "all/0", modelType, shardId)
+
+  /**
+    * Build the path to the best model coefficients file
+    *
+    * @param outputDir output base directory
+    * @param modelType model type (e.g. "fixed-effect", "random-effect")
+    * @param shardId the shard id designator
+    * @return full path to model coefficients file
+    */
+  def bestModelPath(outputDir: String, modelType: String, shardId: String): Path =
+    modelPath(outputDir, "best", modelType, shardId)
 }
--- photon-ml/src/main/scala/com/linkedin/photon/ml/io/ModelOutputMode.scala
+++ photon-ml/src/main/scala/com/linkedin/photon/ml/io/ModelOutputMode.scala
@@ -15,11 +15,9 @@
 package com.linkedin.photon.ml.io
 
 /**
- * Enum of the keys that are expected to be present in the maps in the user-specified constraint string
- * The "name" and "term" keys are mandatory while one of "lowerBound" and "upperBound" is allowed to be skipped in
- * which case it is assumed to be -Inf / +Inf appropriately
- */
-object ConstraintMapKeys extends Enumeration {
-  type ConstraintMapKeys = Value
-  val name, term, lowerBound, upperBound = Value
+  * Supported options for model output.
+  */
+object ModelOutputMode extends Enumeration {
+  type ModelOutputMode = Value
+  val ALL, BEST, NONE = Value
 }
