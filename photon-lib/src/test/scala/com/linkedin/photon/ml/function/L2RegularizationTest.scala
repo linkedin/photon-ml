0ee86ba7b711146965e79469681729ffd1c94b18
Alex Shelkovnykov <ashelkov@ashelkov-ld2.linkedin.biz>
2019-10-26 19:41:34 -0700

    Fix bug which would keep L2 component of gradient and Hessian for intercept term

--- photon-lib/src/test/scala/com/linkedin/photon/ml/function/L2RegularizationTest.scala
+++ photon-lib/src/test/scala/com/linkedin/photon/ml/function/L2RegularizationTest.scala
@@ -14,7 +14,7 @@
  */
 package com.linkedin.photon.ml.function
 
-import breeze.linalg.{DenseMatrix, DenseVector, Vector, diag}
+import breeze.linalg.{DenseMatrix, DenseVector, SparseVector, Vector, diag}
 import org.mockito.Mockito._
 import org.testng.annotations.Test
 import org.testng.Assert.assertEquals
@@ -46,33 +46,58 @@ class L2RegularizationTest {
       l2RegWeight = regularizationWeight
     }
 
-    val mockObjectiveFunctionWithInterceptIndex = new MockObjectiveFunction with L2Regularization {
+    val mockObjectiveFunctionWithIntercept = new MockObjectiveFunction with L2RegularizationTwiceDiff {
       l2RegWeight = regularizationWeight
 
       override def interceptOpt: Option[Int] = mockInterceptIndexOpt
     }
 
-    // Assume that coefficients = 1-vector, multiply = 2-vector for all expected values below
+    // L2 norm of 1-vector of dimension d is d
     val expectedValue = MockObjectiveFunction.VALUE + (regularizationWeight * DIMENSION / 2)
-    val expectedValueWithoutIntercept = MockObjectiveFunction.VALUE + (regularizationWeight * (DIMENSION - 1) / 2)
-    val expectedGradient = DenseVector(Array.fill(DIMENSION)(MockObjectiveFunction.GRADIENT + regularizationWeight))
+    val expectedValueWithIntercept = MockObjectiveFunction.VALUE + (regularizationWeight * (DIMENSION - 1) / 2)
+
+    // L2 gradient if coefficients * L2 weight, but in this case all coefficients are 1
+    val expectedGradient = DenseVector.fill(DIMENSION)(MockObjectiveFunction.GRADIENT + regularizationWeight)
+    val expectedGradientWithIntercept =
+      expectedGradient -:- SparseVector[Double](DIMENSION)((INTERCEPT_INDEX, regularizationWeight))
+
+    // L2 gradient of (H * v) is (v * L2 weight), and in this case v_i is 2 for all i
     val expectedVector =
-      DenseVector(Array.fill(DIMENSION)(MockObjectiveFunction.HESSIAN_VECTOR + (2D * regularizationWeight)))
+      DenseVector.fill(DIMENSION)(MockObjectiveFunction.HESSIAN_VECTOR + (2D * regularizationWeight))
+    val expectedVectorWithIntercept =
+      expectedVector -:- SparseVector[Double](DIMENSION)((INTERCEPT_INDEX, 2D * regularizationWeight))
+
     val expectedDiagonal =
-      DenseVector(Array.fill(DIMENSION)(MockObjectiveFunction.HESSIAN_DIAGONAL + regularizationWeight))
-    val expectedMatrix =
-      diag(DenseVector(Array.fill(DIMENSION)(MockObjectiveFunction.HESSIAN_MATRIX + regularizationWeight)))
+      DenseVector.fill(DIMENSION)(MockObjectiveFunction.HESSIAN_DIAGONAL + regularizationWeight)
+    val expectedDiagonalWithIntercept =
+      expectedDiagonal -:- SparseVector[Double](DIMENSION)((INTERCEPT_INDEX, regularizationWeight))
+
+    val expectedMatrix = diag(DenseVector.fill(DIMENSION)(MockObjectiveFunction.HESSIAN_MATRIX + regularizationWeight))
+    val expectedMatrixWithIntercept =
+      expectedMatrix -:- diag(SparseVector[Double](DIMENSION)((INTERCEPT_INDEX, regularizationWeight)))
 
     assertEquals(mockObjectiveFunction.value(Unit, coefficients, mockNormalization), expectedValue)
     assertEquals(
-      mockObjectiveFunctionWithInterceptIndex.value(Unit, coefficients, mockNormalization),
-      expectedValueWithoutIntercept)
+      mockObjectiveFunctionWithIntercept.value(Unit, coefficients, mockNormalization),
+      expectedValueWithIntercept)
+
     assertEquals(mockObjectiveFunction.gradient(Unit, coefficients, mockNormalization), expectedGradient)
+    assertEquals(
+      mockObjectiveFunctionWithIntercept.gradient(Unit, coefficients, mockNormalization),
+      expectedGradientWithIntercept)
+
     assertEquals(
       mockObjectiveFunction.hessianVector(Unit, coefficients, multiplyVector, mockNormalization),
       expectedVector)
+    assertEquals(
+      mockObjectiveFunctionWithIntercept.hessianVector(Unit, coefficients, multiplyVector, mockNormalization),
+      expectedVectorWithIntercept)
+
     assertEquals(mockObjectiveFunction.hessianDiagonal(Unit, coefficients), expectedDiagonal)
+    assertEquals(mockObjectiveFunctionWithIntercept.hessianDiagonal(Unit, coefficients), expectedDiagonalWithIntercept)
+
     assertEquals(mockObjectiveFunction.hessianMatrix(Unit, coefficients), expectedMatrix)
+    assertEquals(mockObjectiveFunctionWithIntercept.hessianMatrix(Unit, coefficients), expectedMatrixWithIntercept)
   }
 }
 
@@ -96,33 +121,32 @@ object L2RegularizationTest {
     override protected[ml] def value(
         input: Data,
         coefficients: Coefficients,
-        normalizationContext: BroadcastWrapper[NormalizationContext]): Double =
-      calculate(input, coefficients, normalizationContext)._1
+        normalizationContext: BroadcastWrapper[NormalizationContext]): Double = VALUE
 
     override protected[ml] def gradient(
         input: Data,
         coefficients: Coefficients,
         normalizationContext: BroadcastWrapper[NormalizationContext]): Vector[Double] =
-      calculate(input, coefficients, normalizationContext)._2
+      DenseVector.fill(coefficients.length)(GRADIENT)
 
     override protected[ml] def calculate(
         input: Data,
         coefficients: Coefficients,
         normalizationContext: BroadcastWrapper[NormalizationContext]): (Double, Vector[Double]) =
-      (VALUE, DenseVector(Array.fill(coefficients.length)(GRADIENT)))
+      (VALUE, DenseVector.fill(coefficients.length)(GRADIENT))
 
     override protected[ml] def hessianVector(
         input: Data,
         coefficients: Coefficients,
         multiplyVector: Coefficients,
         normalizationContext: BroadcastWrapper[NormalizationContext]): Vector[Double] =
-      DenseVector(Array.fill(coefficients.length)(HESSIAN_VECTOR))
+      DenseVector.fill(coefficients.length)(HESSIAN_VECTOR)
 
     override protected[ml] def hessianDiagonal(input: Data, coefficients: Coefficients): Vector[Double] =
-      DenseVector(Array.fill(coefficients.length)(HESSIAN_DIAGONAL))
+      DenseVector.fill(coefficients.length)(HESSIAN_DIAGONAL)
 
     override protected[ml] def hessianMatrix(input: Data, coefficients: Coefficients): DenseMatrix[Double] =
-      diag(DenseVector(Array.fill(coefficients.length)(HESSIAN_MATRIX)))
+      diag(DenseVector.fill(coefficients.length)(HESSIAN_MATRIX))
 
     override protected[ml] def convertToVector(coefficients: Coefficients): Vector[Double] = coefficients
 
